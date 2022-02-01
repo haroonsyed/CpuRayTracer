75ee5073ba9d36582acbc7ae7e056ffaca10fd86
Haroon Syed <51543523+haroonsyed@users.noreply.github.com>
2022-02-01 00:52:49 -0500

    All systems operational

--- tests/tests.h
+++ tests/tests.h
@@ -12,7 +12,7 @@ TEST_CASE("Adding Points") {
 	Point result = p1 + p2;
 	REQUIRE(aboutEquals(result.x, 2));
 	REQUIRE(aboutEquals(result.y, 2));
-	REQUIRE(aboutEquals(result.z, 3));
+	REQUIRE(aboutEquals(result.z, 2));
 
 	p1 = Point(1.5, 5, 7);
 	p2 = Point(2, 4.8, 5);
