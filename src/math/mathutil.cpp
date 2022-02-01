504b87f22c697ccad7cfa9a94ad163c45c61d5c8
Haroon Syed <51543523+haroonsyed@users.noreply.github.com>
2022-01-31 22:51:37 -0500

    Cmake system now works.

--- src/math/mathutil.cpp
+++ src/math/mathutil.cpp
@@ -2,9 +2,9 @@
 #include <math.h>
 
 bool aboutEquals(double n1, double n2) {
-	return abs(n1-n2) < defaultEpsilon;
+	return fabs(n1-n2) < defaultEpsilon;
 }
 
 bool aboutEquals(double n1, double n2, double epsilon) {
-	return abs(n1 - n2) < epsilon;
+	return fabs(n1 - n2) < epsilon;
 }
